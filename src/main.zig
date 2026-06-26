187c43d879bae615ccf284e2dad5ca20704bae22
Saleem <noreply@madsolame.com>
2026-06-26 18:48:08 +0200

    cli load from file

--- src/main.zig
+++ src/main.zig
@@ -9,6 +9,7 @@ const CliContext = struct {
     generations: u16 = 100,
     disable_render: bool = false,
     sleep_ms: u8 = 60,
+    seed_file: ?[]const u8 = null,
 };
 
 fn parseArgs(args: []const []const u8) !CliContext {
@@ -32,12 +33,22 @@ fn parseArgs(args: []const []const u8) !CliContext {
         if (std.mem.startsWith(u8, arg, "--sleep=")) {
             cli.sleep_ms = @intCast(try extractNumArg("sleep", arg));
         }
+        if (std.mem.startsWith(u8, arg, "--seed=")) {
+            cli.seed_file = arg["--seed=".len..];
+        }
     }
     return cli;
 }
 
 const ProgramArgsError = error{
     InvalidArg,
+};
+
+const ProgramError = error{
+    FileOpenFailed,
+    FileTooBig,
+    GridColumnMismatch,
+    InvalidGridValue,
 };
 
 fn extractNumArg(name: []const u8, arg: []const u8) !u16 {
@@ -52,9 +63,40 @@ pub fn main(init: std.process.Init) !void {
     const allocator: std.mem.Allocator = init.arena.allocator();
     const args = try init.minimal.args.toSlice(allocator);
     const cli = try parseArgs(args);
-    var playground = try game.Playground.random(allocator, init.io, cli.rows, cli.columns);
-
     const io = init.io;
+    var playground: game.Playground = undefined;
+    if (cli.seed_file != null) {
+        const file = std.Io.Dir.openFile(std.Io.Dir.cwd(), io, cli.seed_file.?, .{}) catch {
+            return ProgramError.FileOpenFailed;
+        };
+        defer file.close(io);
+        var buff: [1024]u8 = undefined;
+        const n = file.readPositionalAll(io, &buff, 0) catch {
+            return ProgramError.FileTooBig;
+        };
+        var inputBuff = try std.ArrayList(u1).initCapacity(allocator, cli.rows * cli.columns);
+        for (buff[0..n]) |i| {
+            var value: u1 = 0;
+            switch (i) {
+                '\n' => continue,
+                '-' => value = 0, // dead
+                '0' => value = 1, // alive
+                else => {
+                    return ProgramError.InvalidGridValue;
+                },
+            }
+            try inputBuff.append(allocator, value);
+        }
+        const u1buff = try inputBuff.toOwnedSlice(allocator);
+        if (cli.columns * cli.rows != u1buff.len) {
+            return ProgramError.GridColumnMismatch;
+        }
+        inputBuff.deinit(allocator);
+        playground = try game.Playground.fromBuffer(allocator, cli.rows, cli.columns, u1buff);
+        allocator.free(u1buff);
+    } else {
+        playground = try game.Playground.random(allocator, init.io, cli.rows, cli.columns);
+    }
     var stdout_buffer: [1024]u8 = undefined;
     var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
     const stdout_writer = &stdout_file_writer.interface;

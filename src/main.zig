4656f79cdfb9a59b724ea06ae2efb3a7f76539e6
Ahmad Saleem <azsaleem11@gmail.com>
2026-06-26 18:58:05 +0200

    feat: introduce zig implementation (#2)
    * add zig, created Cell
    
    * added playground, printing
    
    * adjust random to use time
    
    * refactor
    
    * cleanup
    
    * add generations
    
    * print in loop
    
    * add test for neigbours
    
    * add test for generations
    
    * refactor
    
    * refactor
    
    * cli args
    
    * fix args, add debug log
    
    * update field max size
    
    * optimize neighbor loop
    
    * minot refactor
    
    * optimize loops
    
    * refactor
    
    * wip u1
    
    * optimize neighbor calc
    
    * profile
    
    * remove conditionals from neigbor counting
    
    * simplify ops
    
    * sample
    
    * cli load from file
    
    * fix rendering
    
    ---------
    
    Co-authored-by: Saleem <noreply@madsolame.com>

---
+++ src/main.zig
@@ -0,0 +1,116 @@
+const std = @import("std");
+const Io = std.Io;
+const assert = std.debug.assert;
+const game = @import("conways_gameoflife");
+
+const CliContext = struct {
+    rows: u16 = 25,
+    columns: u16 = 25,
+    generations: u16 = 100,
+    disable_render: bool = false,
+    sleep_ms: u8 = 60,
+    seed_file: ?[]const u8 = null,
+};
+
+fn parseArgs(args: []const []const u8) !CliContext {
+    var cli = CliContext{};
+    for (args) |arg| {
+        if (std.mem.startsWith(u8, arg, "--rows=")) {
+            const num = try extractNumArg("rows", arg);
+            cli.rows = num;
+        }
+        if (std.mem.startsWith(u8, arg, "--columns=")) {
+            const num = try extractNumArg("columns", arg);
+            cli.columns = num;
+        }
+        if (std.mem.startsWith(u8, arg, "--generations=")) {
+            const num = try extractNumArg("generations", arg);
+            cli.generations = num;
+        }
+        if (std.mem.eql(u8, arg, "--no-render")) {
+            cli.disable_render = true;
+        }
+        if (std.mem.startsWith(u8, arg, "--sleep=")) {
+            cli.sleep_ms = @intCast(try extractNumArg("sleep", arg));
+        }
+        if (std.mem.startsWith(u8, arg, "--seed=")) {
+            cli.seed_file = arg["--seed=".len..];
+        }
+    }
+    return cli;
+}
+
+const ProgramArgsError = error{
+    InvalidArg,
+};
+
+const ProgramError = error{
+    FileOpenFailed,
+    FileTooBig,
+    GridColumnMismatch,
+    InvalidGridValue,
+};
+
+fn extractNumArg(name: []const u8, arg: []const u8) !u16 {
+    if (arg.len < name.len + 3) { //-- <name> = <value>
+        return ProgramArgsError.InvalidArg;
+    }
+    const value = arg[name.len + 3 .. arg.len];
+    return std.fmt.parseInt(u16, value, 10);
+}
+
+pub fn main(init: std.process.Init) !void {
+    const allocator: std.mem.Allocator = init.arena.allocator();
+    const args = try init.minimal.args.toSlice(allocator);
+    const cli = try parseArgs(args);
+    const io = init.io;
+    var playground: game.Playground = undefined;
+    if (cli.seed_file != null) {
+        const file = std.Io.Dir.openFile(std.Io.Dir.cwd(), io, cli.seed_file.?, .{}) catch {
+            return ProgramError.FileOpenFailed;
+        };
+        defer file.close(io);
+        var buff: [1024]u8 = undefined;
+        const n = file.readPositionalAll(io, &buff, 0) catch {
+            return ProgramError.FileTooBig;
+        };
+        var inputBuff = try std.ArrayList(u1).initCapacity(allocator, cli.rows * cli.columns);
+        for (buff[0..n]) |i| {
+            var value: u1 = 0;
+            switch (i) {
+                '\n' => continue,
+                '-' => value = 0, // dead
+                '0' => value = 1, // alive
+                else => {
+                    return ProgramError.InvalidGridValue;
+                },
+            }
+            try inputBuff.append(allocator, value);
+        }
+        const u1buff = try inputBuff.toOwnedSlice(allocator);
+        if (cli.columns * cli.rows != u1buff.len) {
+            return ProgramError.GridColumnMismatch;
+        }
+        inputBuff.deinit(allocator);
+        playground = try game.Playground.fromBuffer(allocator, cli.rows, cli.columns, u1buff);
+        allocator.free(u1buff);
+    } else {
+        playground = try game.Playground.random(allocator, init.io, cli.rows, cli.columns);
+    }
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
+    const stdout_writer = &stdout_file_writer.interface;
+    std.debug.print("running with args: #{}\n", .{cli});
+    for (0..cli.generations) |_| {
+        if (!cli.disable_render) {
+            try playground.print(stdout_writer);
+            try stdout_writer.flush();
+        }
+        playground.nextGen();
+        if (cli.sleep_ms > 0) {
+            try std.Io.sleep(init.io, std.Io.Duration.fromMilliseconds(cli.sleep_ms), std.Io.Clock.real);
+        }
+        if (cli.disable_render) continue;
+        try playground.clearPrint(stdout_writer);
+    }
+}
---
+++ src/root.zig
@@ -0,0 +1,170 @@
+const std = @import("std");
+const Io = std.Io;
+const assert = std.debug.assert;
+
+pub const Cell = struct {
+    value: u1 = 0,
+
+    fn setAlive(self: *Cell, alive: bool) void {
+        self.value = @intFromBool(alive);
+    }
+};
+
+pub const Playground = struct {
+    rows: u32,
+    columns: u32,
+    grid: []Cell,
+    swap: []Cell = undefined,
+
+    fn neighborLifeCount(self: *const Playground, row: u32, col: u32) u4 {
+        assert(self.rows > row);
+        assert(self.columns > col);
+        const has_prev_row: u32 = @intFromBool(row > 0);
+        const has_next_row: u32 = @intFromBool(row + 1 < self.rows);
+        const has_prev_col: u32 = @intFromBool(col > 0);
+        const has_next_col: u32 = @intFromBool(col + 1 < self.columns);
+        var neighbors: u32 = 0;
+        neighbors += self.grid[(row - 1 * has_prev_row) * self.columns + col - 1 * has_prev_col].value * (has_prev_row & has_prev_col);
+        neighbors += self.grid[(row - 1 * has_prev_row) * self.columns + col].value * has_prev_row;
+        neighbors += self.grid[(row - 1 * has_prev_row) * self.columns + col + 1 * has_next_col].value * (has_prev_row & has_next_col);
+
+        neighbors += self.grid[row * self.columns + col - 1 * has_prev_col].value * has_prev_col;
+        neighbors += self.grid[row * self.columns + col + 1 * has_next_col].value * has_next_col;
+
+        neighbors += self.grid[(row + 1 * has_next_row) * self.columns + col - 1 * has_prev_col].value * (has_next_row & has_prev_col);
+        neighbors += self.grid[(row + 1 * has_next_row) * self.columns + col].value * has_next_row;
+        neighbors += self.grid[(row + 1 * has_next_row) * self.columns + col + 1 * has_next_col].value * (has_next_row & has_next_col);
+
+        return @intCast(neighbors);
+    }
+
+    pub fn print(self: *const Playground, writer: *std.Io.Writer) !void {
+        for (0..self.rows) |row_index| {
+            for (0..self.columns) |col_index| {
+                const cell = self.grid[row_index * self.columns + col_index];
+                const display: u16 = switch (cell.value) {
+                    0 => ' ',
+                    1 => '\u{2593}',
+                };
+                try writer.printUnicodeCodepoint(display);
+            }
+            try writer.printAsciiChar('\n', std.fmt.Options{});
+        }
+    }
+
+    pub fn nextGen(self: *Playground) void {
+        for (0..self.rows) |row_index| {
+            for (0..self.columns) |col_index| {
+                var cell = self.grid[self.cellIndex(row_index, col_index)];
+                switch (self.neighborLifeCount(@intCast(row_index), @intCast(col_index))) {
+                    0...1 => cell.setAlive(false),
+                    2 => {},
+                    3 => cell.setAlive(true),
+                    else => cell.setAlive(false),
+                }
+                self.swap[self.cellIndex(row_index, col_index)] = cell;
+            }
+        }
+        const temp = self.grid;
+        self.grid = self.swap;
+        self.swap = temp;
+    }
+
+    fn cellIndex(self: *const Playground, row_index: usize, col_index: usize) usize {
+        return row_index * self.columns + col_index;
+    }
+
+    //
+    // 1. Print multiline block once.
+    // 2. Remember how many terminal rows it used.
+    // 3. On the next update, move the cursor back up that many rows.
+    // 4. Clear those rows.
+    // 5. Print the new content in the same space.
+    //
+    // \x1b[{n}F   move cursor up n lines, to column 0
+    // \x1b[{n}A   move cursor up n lines, same column
+    // \x1b[2K     clear the current line
+    // \x1b[J      clear from cursor to end of screen
+    // \x1b[?25l   hide cursor
+    // \x1b[?25h   show cursor
+
+    pub fn clearPrint(self: *const Playground, w: *std.Io.Writer) !void {
+        try w.print("\x1b[{d}F", .{self.rows});
+        for (0..self.rows) |_| {
+            try w.print("\x1b[2K\n", .{});
+        }
+        try w.print("\x1b[{d}F", .{self.rows});
+    }
+
+    pub fn deinit(self: *Playground, allocator: std.mem.Allocator) void {
+        allocator.free(self.grid);
+        allocator.free(self.swap);
+    }
+
+    pub fn new(allocator: std.mem.Allocator, rows: u32, columns: u32) !Playground {
+        const grid = try allocator.alloc(Cell, rows * columns);
+        const swap = try allocator.alloc(Cell, rows * columns);
+        return Playground{ .rows = rows, .columns = columns, .grid = grid, .swap = swap };
+    }
+
+    pub fn fromBuffer(allocator: std.mem.Allocator, rows: u32, columns: u32, buff: []u1) !Playground {
+        assert(buff.len == rows * columns);
+        const playground = try Playground.new(allocator, rows, columns);
+        for (0..rows * columns) |pos| {
+            playground.grid[pos].value = buff[pos];
+        }
+        return playground;
+    }
+
+    pub fn random(allocator: std.mem.Allocator, io: std.Io, rows: u32, columns: u32) !Playground {
+        var r = std.Random.DefaultPrng.init(@intCast(std.Io.Clock.real.now(io).toMilliseconds()));
+
+        var playground = try Playground.new(allocator, rows, columns);
+        for (0..rows * columns) |i| {
+            const mycellval = r.random().uintAtMost(u1, 1);
+            playground.grid[i] = Cell{ .value = mycellval };
+        }
+        return playground;
+    }
+};
+
+test "playground neighbors works" {
+    var stable = [_]u1{
+        0, 1, 0, // 0x0
+        1, 0, 1, // x0x
+        0, 1, 0, // 0x0
+    };
+    // next generation should basically be the same
+    var playgound = try Playground.fromBuffer(std.testing.allocator, 3, 3, stable[0..]);
+    defer playgound.deinit(std.testing.allocator);
+    try std.testing.expectEqual(2, playgound.neighborLifeCount(0, 0));
+    try std.testing.expectEqual(2, playgound.neighborLifeCount(0, 1));
+    try std.testing.expectEqual(2, playgound.neighborLifeCount(0, 2));
+    try std.testing.expectEqual(2, playgound.neighborLifeCount(1, 0));
+    try std.testing.expectEqual(4, playgound.neighborLifeCount(1, 1));
+    try std.testing.expectEqual(2, playgound.neighborLifeCount(1, 2));
+    try std.testing.expectEqual(2, playgound.neighborLifeCount(2, 0));
+    try std.testing.expectEqual(2, playgound.neighborLifeCount(2, 1));
+    try std.testing.expectEqual(2, playgound.neighborLifeCount(2, 2));
+}
+
+test "playground next generation works" {
+    var stable = [_]u1{
+        0, 1, 0, // 0x0
+        0, 1, 0, // 0x0
+        0, 1, 0, // 0x0
+    };
+    const stableSlice: []u1 = stable[0..];
+    // next generation should basically be the same
+    var playgound = try Playground.fromBuffer(std.testing.allocator, 3, 3, stableSlice);
+    defer playgound.deinit(std.testing.allocator);
+    playgound.nextGen();
+    var expected = [_]u1{
+        0, 0, 0, // 000
+        1, 1, 1, // 111
+        0, 0, 0, // 000
+    };
+    var playgound2 = try Playground.fromBuffer(std.testing.allocator, 3, 3, expected[0..]);
+    defer playgound2.deinit(std.testing.allocator);
+    try std.testing.expectEqualSlices(Cell, playgound2.grid, playgound.grid);
+}
